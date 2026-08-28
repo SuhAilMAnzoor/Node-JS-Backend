63dba2b20f0190fde00df377d973b7f0efffb6c6
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-08-28 19:28:53 +0500

    build a server on NodeJS to understand backend with simple backend project

--- basic-nodejs-project/index.js
+++ basic-nodejs-project/index.js
@@ -6,7 +6,7 @@ const express = require('express');  // common js
 // now app is the main object that handles all server logic
 const app = express()
 
-const port = 5000    // virtual port where the server will listen
+// const port = 5000    // virtual port where the server will listen
 
 app.get('/', (req, res) => {
     res.send('Hello, Welcome to My Website!')   // 
