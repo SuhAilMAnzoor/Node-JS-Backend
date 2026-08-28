a63562c33cf3eeac1e3bf4250d5e85461b1d750c
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-08-28 19:26:25 +0500

    added dotenv file to separate application configuration and sensitive data from the source code

--- basic-nodejs-project/index.js
+++ basic-nodejs-project/index.js
@@ -21,6 +21,6 @@ app.get('/aboutme', function(req,res) {
 })
 
 
-app.listen(port, () => { // set port on the basis of .env file 
+app.listen(process.env.PORT, () => { // set port on the basis of .env file 
     console.log(`Example app listening on port ${port}`)
 })
