61ed513acd2a52bb2aca53fcf5a4cdea3e9879aa
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-09-11 23:59:55 +0500

    Full Stack Application connecting with backend, how frontend will be connect with backend

---
+++ fullstackbasic/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+
+  return (
+    <>
+    <h1>Full Stack Application</h1>
+    <h2>Connecting with backend</h2>
+    </>
+  )
+}
+
+export default App
