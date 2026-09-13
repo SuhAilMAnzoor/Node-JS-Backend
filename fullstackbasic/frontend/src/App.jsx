b916d76ef1a1dff221ce9bf304aec1a6c56ccd3a
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-09-13 18:38:01 +0500

    fetch and display jokes from backend API with axios

--- fullstackbasic/frontend/src/App.jsx
+++ fullstackbasic/frontend/src/App.jsx
@@ -2,21 +2,35 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios';
 
+// fetch, axios, react-query what are the requiremnt first you have to understand
+// in this we use axios to fetch data from backend server and display it in frontend
+// this library is used to make HTTP requests from the browser and Node.js. It provides a simple and easy-to-use API for sending asynchronous HTTP requests to REST endpoints and performing CRUD operations.
+
 function App() {
   const [jokes, setJokes] = useState([])
 
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data)
+  }
+  ).catch((error) => {
+    console.log(error)
+  })
+})
   return (
     <>
     <h1>Full Stack Application</h1>
     <p>JOKES: {jokes.length}</p>
 
     {
-      jokes.map((joke, index) => {
+      jokes.map((joke, index) => (
         <div key={joke.id}>
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
         </div>
-      })
+      ))
      }
     </>
   )
--- fullstackbasic/frontend/vite.config.js
+++ fullstackbasic/frontend/vite.config.js
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      '/api' : 'http://localhost:3000'
+    }
+  },
+  plugins: [
+    react(),
+  ],
 })
